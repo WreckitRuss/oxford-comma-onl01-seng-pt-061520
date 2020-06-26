ec503993ba1c09fc161804fdbf908ed4ad4b4ce8
Russell Doane <russdoane@yahoo.com>
2020-06-26 16:08:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+  def oxford_comma array
+    if array.length==1 
+        puts array[0]
+    else 
+        last=array[array.length-1] 
+        array.pop 
+        string=array.join(', ')
+        if array.length==1
+            string.concat(" and ")
+        else
+            string.concat(", and ")
+        end
+        string.concat(last)
+        puts string 
+    end
+end
+
+items = ["kiwi","durian","start fruit","mangos","dragon fruit"]
+oxford_comma(items) 
 
 end
